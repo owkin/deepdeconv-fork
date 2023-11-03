1e4cf838a90767b07387bb8edb0250f03b2c33c8
Khalil Ouardini <ouardini.k@gmail.com>
2023-11-03 10:07:15 +0000

    create scripts folder

---
+++ scripts/create_signature_matrix/example_files/signatureBreast-Lung.r
@@ -0,0 +1,240 @@
+### from Almudena's script : project/processing_CHUV/omics-toolbox/GeoMX_WTA/workflow/utils_and_notebooks/Almudena/Deconvolution/Signature_lung.r
+# the only difference is the granularity of cell types
+
+#library(anndata)
+library(Seurat)
+
+library(dplyr)
+library(plyr)
+library(ggforce)
+library(ggplot2)
+library(reshape2) 
+library(ComplexHeatmap)
+library(corrplot)
+library(caret)
+
+library(DeconRNASeq)
+library(nnls)
+library(FARDEEP)
+library(MIND)
+source("~/project/processing_CHUV/omics-toolbox/GeoMX_WTA/workflow/utils_and_notebooks/Almudena/Deconvolution/Deconvolution_function.R")
+source("~/project/processing_CHUV/omics-toolbox/GeoMX_WTA/workflow/utils_and_notebooks/Almudena/Deconvolution/Signature_function.R")
+
+
+dir_out <- "~/project/sandbox-SCa/deconvolution-signature/output/Breast-Lung"
+dir.create(dir_out)
+
+dirsc <- "~/project/processing_CHUV/omics-toolbox/GeoMX_WTA/workflow/utils_and_notebooks/Almudena/Deconvolution/Output/Signature/scrnaseq"
+expr_obj <- Read10X(dirsc)
+metadata <- read.table(file.path(dirsc,"metadata.csv"),sep=",",row.names=1,header=TRUE)
+expr <- CreateSeuratObject(counts=expr_obj,meta.data=metadata)
+#expr <- subset(x = expr, subset = tissue == "breast")
+
+# remove  TNK_dividing (not enough specific)
+# merge together DC and DC ativated but keep pDC apart
+expr <- subset(x = expr, subset = Celltype != "TNK_dividing")
+
+expr$Celltype2 <- ifelse(grepl("Tu",expr$Celltype),"Tumor",expr$Celltype)
+# expr$Celltype2 <- ifelse(grepl("T_",expr$Celltype2),"TNK",expr$Celltype2)
+# expr$Celltype2 <- ifelse(grepl("\\bNK\\b",expr$Celltype2),"TNK",expr$Celltype2)
+# expr$Celltype2 <- ifelse(grepl("\\bTNK_dividing\\b",expr$Celltype2),"TNK",expr$Celltype2)
+expr$Celltype2 <- ifelse(grepl("\\bDC_activated\\b",expr$Celltype2),"DC",expr$Celltype2)
+# expr$Celltype2 <- ifelse(grepl("\\bDC_plasmacytoid\\b",expr$Celltype2),"DC",expr$Celltype2)
+expr$Celltype2 <- ifelse(grepl("\\bPericyte\\b",expr$Celltype2),"Stroma",expr$Celltype2)
+expr$Celltype2 <- ifelse(grepl("\\bFibro_muscle\\b",expr$Celltype2),"Stroma",expr$Celltype2)
+
+
+# Removing mitochondrial and ribosomal genes
+#expr2 <- NormalizeData(object = expr2, normalization.method = "RC",scale.factor = 1e6)
+genes.ribomit <- grep(pattern = "^RP[SL][[:digit:]]|^RP[[:digit:]]|^RPSA|^RPS|^RPL|^MT-|^MRPL",rownames(expr))
+dim(expr) 
+expr <- expr[-c(genes.ribomit),]
+dim(expr) 
+# remove housekeeping genes and patient specific ones: ACTB, if only this one, not a big deal
+genes2remove = grep(pattern = "^ACTB$|TMSB4X|IGKC|^IG[HL]",rownames(expr))
+expr <- expr[-c(genes2remove),]
+dim(expr)
+
+# Split dataset into 2
+trainIndex <- createDataPartition(as.character(expr$Celltype2), p = .5)
+scRNseq_t <- expr[,unlist(trainIndex)]
+
+scRNseq_test <- expr[,-unlist(trainIndex)]
+
+
+# Differential expression analysis
+#scRNseq_seurat <- CreateSeuratObject(scRNseq_t, project = "Signature")
+Idents(scRNseq_t) <- scRNseq_t$Celltype2
+print(table(Idents(scRNseq_t)))
+if(!file.exists(file.path(dir_out,paste0("DE_",unique(Idents(scRNseq_t))[length(unique(Idents(scRNseq_t)))],".txt")))){
+    DGE_celltypes(scRNseq_t,Idents(scRNseq_t),file.path(dir_out))
+}
+
+
+if(!file.exists(file.path(dir_out,"Chr_CHUV.txt"))){
+
+  scRNseq_t <- NormalizeData(object = scRNseq_t, normalization.method = "RC",scale.factor = 10000)
+
+  signature <- buildSignatureMatrix_Seurat("Chr_CHUV",
+      scRNseq_t,Idents(scRNseq_t),file.path(dir_out),
+      pvaladj.cutoff=0.05,diff.cutoff=0.5,
+      minG=50,maxG=200)
+  write.table(signature,file.path(dir_out,"Chr_CHUV.txt"),sep="\t",row.names=TRUE,col.names=NA)
+  
+}else{
+  signature <- read.table(file.path(dir_out,"Chr_CHUV.txt"),sep="\t",row.names=1,header=TRUE)
+}
+
+################ Testing signature
+
+#scRNseq_test <- NormalizeData(object = scRNseq_test, normalization.method = "RC",scale.factor = 10000)
+scRNseq_test <- NormalizeData(object = scRNseq_test, normalization.method = "LogNormalize",scale.factor = 10000)
+
+Idents(scRNseq_test) <- scRNseq_test$Celltype2
+print(table(Idents(scRNseq_test)))
+
+#### Sanity check 1
+dir.create(file.path(dir_out,"Sanity_Check_1"), recursive = TRUE, showWarnings = FALSE)
+T_test <- AverageExpression(object = scRNseq_test,slot="data")
+T_test <- as.data.frame(T_test)
+colnames(T_test) <- gsub("RNA.","",colnames(T_test))
+#T_test <- 2^T_test
+
+# IDsCells <- unique(Idents(scRNseq_t))
+# T_test <- c()
+# for(celltype in IDsCells){
+#   T_test <- cbind(T_test,apply(scRNseq_test,1,function(y) mean(y[which(Idents(scRNseq_t)==celltype)]))) #to try both median and mean
+#  }
+# colnames(T_test) <- IDsCells
+out <- Deconvolution(T_test,as.data.frame(signature))
+
+
+out2 <- do.call(rbind.data.frame, out)
+out2 <- melt(as.matrix(out2))
+out2$Mix <- gsub(".*\\.","",out2[,1])
+out2[,1] <- gsub("\\..*","",out2[,1])
+colnames(out2) <- c("Method","CellType","Fraction","Mix")
+out2 <- out2[which(out2$Mix == out2$CellType),]
+
+p <- ggplot(out2, aes(x=CellType, y=Fraction, color=Method)) + 
+  geom_point() + theme_bw() + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+ggsave(file.path(dir_out,"Sanity_Check_1","SC1.png"))
+
+p <- ggplot(out2, aes(x=CellType, y=Fraction, color=Method)) + 
+  geom_point() + theme_bw() + scale_color_brewer(palette="Paired") + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+ggsave(file.path(dir_out,"Sanity_Check_1","SC1_2.png"))
+
+
+summary <- out2 %>%
+    group_by(Method) %>%
+    summarise_at(vars(Fraction), list(name = mean))
+summary <- as.data.frame(summary)
+summary <- summary[order(-summary$name),] 
+write.table(summary,file.path(dir_out,"Sanity_Check_1","summary_frac.txt"),sep="\t",row.names=FALSE,col.names=TRUE)
+
+
+# S = Rodeo(T_test, out[[6]])
+# write.table(S, file.path(dir_out,"Sanity_Check_1",file="EstimatedS_6.txt"), sep="\t", quote=F,row.names=TRUE,col.names=NA)
+
+
+
+#### Sanity check 2
+dir.create(file.path(dir_out,"Sanity_Check_2"), recursive = TRUE, showWarnings = FALSE)
+#colnames(scRNseq_test) <- as.character(Idents(scRNseq_test))
+scRNseq_test <- RenameCells(scRNseq_test, Idents(scRNseq_test))
+
+Nsampl <- 100
+frac <- c()
+T <- c()
+for(i in 1:Nsampl){
+  set.seed(i)
+  CellSub <- sample(colnames(scRNseq_test),1000)
+  scRNseq_testSub <- scRNseq_test[,CellSub]
+  #scRNseq_testSub2 <- as.matrix(scRNseq_testSub)
+  scRNseq_testSub2 <- GetAssayData(object = scRNseq_testSub, assay.type = "RNA", slot = "data")
+  scRNseq_testSub2 <- t(scRNseq_testSub2)
+  frac <- cbind(frac,table(gsub("_C$","",gsub("_G$","",gsub("_T$","",gsub("_A$","",substr(rownames(scRNseq_testSub2), 1, nchar(rownames(scRNseq_testSub2))-27))))))/1000)
+  #frac <- cbind(frac,table(substr(rownames(scRNseq_testSub2), 1, nchar(rownames(scRNseq_testSub2))-27)))
+  scRNseq_testSub2 <- apply(scRNseq_testSub2,2,mean)
+  T <- cbind(T,scRNseq_testSub2)
+}
+fractions <- Deconvolution(T,as.data.frame(signature))
+# S = Rodeo(T, fractions[[6]])
+# write.table(S, file.path(dir_out,"Sanity_Check_2",file="EstimatedS_6.txt"), sep="\t", quote=F,row.names=TRUE,col.names=NA)
+
+frac <- frac[order(match(rownames(frac),rownames(fractions[[1]]))),]
+identical(rownames(frac),rownames(fractions[[1]]))
+correl_all <- c()
+for(i in 1:length(fractions)){
+  for(samp in 1:Nsampl){
+    correl <- cor.test(frac[,samp],fractions[[i]][,samp])$estimate
+    correl_pval <- cor.test(frac[,samp],fractions[[i]][,samp])$p.value
+    correl_all <- rbind(correl_all,c(correl,correl_pval))
+  }
+}
+correl_all <- data.frame(Sample=rep(seq(1:Nsampl),10),Method=rep(names(fractions),Nsampl),Correlation=correl_all[,1],Pvalue=correl_all[,2])
+write.table(correl_all,file.path(dir_out,"Sanity_Check_2","summary_correl.txt"),sep="\t",row.names=FALSE,col.names=TRUE)
+
+p_meds <- ddply(correl_all, .(Method), summarise, med = median(Correlation))
+p <- ggplot(correl_all, aes(x=Method, y=Correlation)) + 
+  geom_boxplot() + theme_bw() + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))+ 
+    geom_text(data = p_meds, aes(x = Method, y = med, label = round(med,4)), size = 3, vjust = -6.5)
+ggsave(file.path(dir_out,"Sanity_Check_2","SC2.png"))
+
+
+#### Sanity check 3
+dir.create(file.path(dir_out,"Sanity_Check_3"), recursive = TRUE, showWarnings = FALSE)
+
+NcellType <- unique(Idents(scRNseq_test))
+for(CT in NcellType){
+  assign(CT,scRNseq_test[,which(Idents(scRNseq_test) == CT)])
+}
+
+Nsampl <- 100
+frac <- c()
+T <- c()
+for(i in 1:100){
+  set.seed(i)
+  Nprop <- runif(length(NcellType),0,1)
+    
+  Ncell <- c() 
+  expr_N <- c()
+  for(CT in NcellType){
+    temp <- eval(parse(text = as.character(CT)))
+    if(round(Nprop[which(NcellType == CT)]*100) == 0){next} 
+    temp <- temp[,sample(colnames(temp),round(Nprop[which(NcellType == CT)]*50))]
+    temp <-  GetAssayData(object = temp, assay.type = "RNA", slot = "data")
+    temp <- t(temp)
+    expr_N <- rbind(expr_N,temp)
+    Ncell <- c(Ncell,nrow(temp)) 
+  }
+  total <- sum(Ncell)
+  Ncell <- Ncell/total
+  frac <- cbind(frac,Ncell)
+
+  Tsub <- apply(expr_N,2,mean)
+  T <- cbind(T,Tsub)
+}
+
+rownames(frac) <- NcellType
+fractions <- Deconvolution(T,as.data.frame(signature))
+# S = Rodeo(T, fractions[[10]])
+# write.table(S, file.path(dir_out,"Sanity_Check_3",file="EstimatedS_10.txt"), sep="\t", quote=F,row.names=TRUE,col.names=NA)
+
+frac <- frac[order(match(rownames(frac),rownames(fractions[[1]]))),]
+identical(rownames(frac),rownames(fractions[[1]]))
+
+correl_all <- c()
+for(i in 1:length(fractions)){
+  for(samp in 1:Nsampl){
+    correl <- cor.test(frac[,samp],fractions[[i]][,samp])$estimate
+    correl_pval <- cor.test(frac[,samp],fractions[[i]][,samp])$p.value
+    correl_all <- rbind(correl_all,c(correl,correl_pval))
+  }
+}
+correl_all <- data.frame(Method=rep(names(fractions),Nsampl),Correlation=correl_all[,1],Pvalue=correl_all[,2])
+write.table(correl_all,file.path(dir_out,"Sanity_Check_3","summary_correl.txt"),sep="\t",row.names=FALSE,col.names=TRUE)
+
+p <- ggplot(correl_all, aes(x=Method, y=Correlation)) + 
+  geom_boxplot() + theme_bw() + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+ggsave(file.path(dir_out,"Sanity_Check_3","SC3.png"))
---
+++ scripts/create_signature_matrix/helpers/Signature_function.R
@@ -0,0 +1,163 @@
+
+### Function to conduct DGE
+DGE_celltypes <- function(expr,clusters,output_dir){
+    for(celltype in unique(clusters)){
+        DEcelltype <- FindMarkers(object=expr, ident.1 = celltype, ident.2 = NULL, only.pos = TRUE, test.use = "bimod")
+        write.table(DEcelltype,file.path(output_dir,paste0("DE_",celltype,".txt")),sep="\t",row.names=TRUE,col.names=NA)
+    }
+}
+
+# ## Function to build signature. similar approach in "buildSignatureMatrixUsingSeurat" from https://github.com/dtsoucas/DWLS/blob/master/Deconvolution_functions.R
+
+buildSignatureMatrix <- function(NameSig,expression,clusters,output_dir,diff.cutoff=NULL,pvaladj.cutoff=NULL,minG=NULL,maxG=NULL){
+  if(is.null(diff.cutoff)){diff.cutoff <- 0.25}
+  if(is.null(pvaladj.cutoff)){pvaladj.cutoff <- 0.05}
+  if(is.null(minG)){minG <- 50}
+  if(is.null(maxG)){maxG <- 200}
+
+  cat(paste("Building signature using the following cutoffs:",diff.cutoff,"for FC,",pvaladj.cutoff,"for adjusted pvalue,",minG,"minimum number of genes and",maxG,"maximum number of genes\n"))
+
+  NGenes<-c()
+  IDsCells <- unique(clusters)
+  for (celltype in IDsCells){
+    DEcelltype <- read.table(file.path(output_dir,paste0("DE_",celltype,".txt")),sep="\t",row.names=1,header=TRUE)
+    DEGenes<-rownames(DEcelltype)[intersect(which(DEcelltype$p_val_adj<pvaladj.cutoff),which(DEcelltype$avg_log2FC>diff.cutoff))]
+    nonMir = grep("MIR|Mir", DEGenes, invert = T)
+    assign(paste0("DE_filt.",celltype),DEcelltype[which(rownames(DEcelltype) %in% DEGenes[nonMir]),])
+    NGenes<-c(NGenes,length(DEGenes[nonMir]))
+  }
+  names(NGenes) <- IDsCells
+  write.table(NGenes,file.path(output_dir,paste0("Overview_Nmarkers_",diff.cutoff,"_",pvaladj.cutoff,"_",minG,"_",maxG,".txt")))
+  #if(max(NGenes) < maxG){maxG <- max(NGenes)} 
+
+  conditionNumbers<-c()
+  ntop <- c()
+  for(G in minG:maxG){
+
+    IDGenes<-c()
+    for(Ind in 1:length(IDsCells)){
+      temp <- eval(parse(text = as.name(paste0("DE_filt.",IDsCells[Ind]))))
+      temp <- temp[order(temp$avg_log2FC,decreasing=TRUE),]       # to try both sorting based on p_pval_adj or avg_log2FC
+      IDGenes <- c(IDGenes,(rownames(temp)[1:min(G,NGenes[Ind])]))
+    }
+
+    IDGenes <- unique(IDGenes)
+    ntop <- c(ntop,length(IDGenes))
+
+    # building signature matrices for each G using top most significant markers
+    ExprSubset <- expression[IDGenes,]
+    if(max(ExprSubset) < 10){ExprSubset <- 2^ExprSubset}
+    Sig <- c()
+    for(celltype in IDsCells){
+      Sig <- cbind(Sig,(apply(ExprSubset,1,function(y) mean(y[which(clusters==celltype)])))) #to try both median and mean
+    }
+    colnames(Sig) <- IDsCells
+    conditionNumbers <- c(conditionNumbers,kappa(Sig))
+  }
+  CN <- data.frame(G=minG:maxG,Ngenes=ntop,ConditionNumber=conditionNumbers)
+  a <- ggplot(CN, aes(x=G, y=ConditionNumber)) + theme_bw() + geom_point() + geom_vline(xintercept = which.min(conditionNumbers) + minG-1, linetype="dotted", color = "blue", size=1.5)
+  ggsave(file.path(output_dir,paste0("Overview_ConditionNumbers_",diff.cutoff,"_",pvaladj.cutoff,"_",minG,"_",maxG,".png")))
+  write.table(CN,file.path(output_dir,paste0("Overview_ConditionNumbers_",diff.cutoff,"_",pvaladj.cutoff,"_",minG,"_",maxG,".txt")))
+
+  Gall <- which.min(conditionNumbers) + minG-1 # The optimal number of DE genes selected
+  cat(paste("The optimal number of genes for the signature is",Gall,"\n"))
+
+  # Build final signature matrix with the most optimal number of genes
+  IDGenes<-c()
+  for(Ind in 1:length(IDsCells)){
+    temp <- eval(parse(text = as.name(paste0("DE_filt.",IDsCells[Ind]))))
+    temp <- temp[order(temp$avg_log2FC,decreasing=TRUE),]
+    IDGenes <- c(IDGenes,(rownames(temp)[1:min(Gall,NGenes[Ind])]))
+   }
+
+  IDGenes <- unique(IDGenes)
+  ExprSubset <- expression[IDGenes,]
+  Sig <- c()
+   for(celltype in IDsCells){
+     Sig <- cbind(Sig,(apply(ExprSubset,1,function(y) mean(y[which(clusters==celltype)])))) #to try both median and mean
+   }
+  colnames(Sig) <- IDsCells
+  write.table(Sig,file.path(output_dir,paste0("Signature_",NameSig,"_",diff.cutoff,"_",pvaladj.cutoff,"_",minG,"_",maxG,".txt")),sep="\t",row.names=TRUE,col.names=NA)
+  png(file.path(output_dir,paste0("Signature_",NameSig,"_",diff.cutoff,"_",pvaladj.cutoff,"_",minG,"_",maxG,".png")))
+    a <- heatmap(Sig)
+  dev.off()
+  return(Sig)
+}
+
+
+buildSignatureMatrix_Seurat <- function(NameSig,expression,clusters,output_dir,diff.cutoff=NULL,pvaladj.cutoff=NULL,minG=NULL,maxG=NULL){
+  if(is.null(diff.cutoff)){diff.cutoff <- 0.25}
+  if(is.null(pvaladj.cutoff)){pvaladj.cutoff <- 0.05}
+  if(is.null(minG)){minG <- 50}
+  if(is.null(maxG)){maxG <- 200}
+
+  cat(paste("Building signature using the following cutoffs:",diff.cutoff,"for FC,",pvaladj.cutoff,"for adjusted pvalue,",minG,"minimum number of genes and",maxG,"maximum number of genes\n"))
+
+  NGenes<-c()
+  IDsCells <- unique(clusters)
+  for (celltype in IDsCells){
+    DEcelltype <- read.table(file.path(output_dir,paste0("DE_",celltype,".txt")),sep="\t",row.names=1,header=TRUE)
+    DEGenes<-rownames(DEcelltype)[intersect(which(DEcelltype$p_val_adj<pvaladj.cutoff),which(DEcelltype$avg_log2FC>diff.cutoff))]
+    nonMir = grep("MIR|Mir", DEGenes, invert = T)
+    assign(paste0("DE_filt.",celltype),DEcelltype[which(rownames(DEcelltype) %in% DEGenes[nonMir]),])
+    NGenes<-c(NGenes,length(DEGenes[nonMir]))
+  }
+  names(NGenes) <- IDsCells
+  write.table(NGenes,file.path(output_dir,paste0("Overview_Nmarkers_",diff.cutoff,"_",pvaladj.cutoff,"_",minG,"_",maxG,".txt")))
+  #if(max(NGenes) < maxG){maxG <- max(NGenes)} 
+
+  conditionNumbers<-c()
+  ntop <- c()
+  for(G in minG:maxG){
+
+    IDGenes<-c()
+    for(Ind in 1:length(IDsCells)){
+      temp <- eval(parse(text = as.name(paste0("DE_filt.",IDsCells[Ind]))))
+      temp <- temp[order(temp$avg_log2FC,decreasing=TRUE),]       # to try both sorting based on p_pval_adj or avg_log2FC
+      IDGenes <- c(IDGenes,(rownames(temp)[1:min(G,NGenes[Ind])]))
+    }
+
+    IDGenes <- unique(IDGenes)
+    ntop <- c(ntop,length(IDGenes))
+
+    # building signature matrices for each G using top most significant markers
+    ExprSubset <- GetAssayData(expression[IDGenes,], assay.type = "RNA", slot = "data")
+    if(max(ExprSubset) < 10){ExprSubset <- 2^ExprSubset} # RNA slot is not available. normalized data is log2 or log10?
+    Sig <- c()
+    for(celltype in IDsCells){
+      Sig <- cbind(Sig,(apply(ExprSubset,1,function(y) mean(y[which(clusters==celltype)])))) #to try both median and mean
+    }
+    colnames(Sig) <- IDsCells
+    conditionNumbers <- c(conditionNumbers,kappa(Sig))
+  }
+  CN <- data.frame(G=minG:maxG,Ngenes=ntop,ConditionNumber=conditionNumbers)
+  a <- ggplot(CN, aes(x=G, y=ConditionNumber)) + theme_bw() + geom_point() + geom_vline(xintercept = which.min(conditionNumbers) + minG-1, linetype="dotted", color = "blue", size=1.5)
+  ggsave(file.path(output_dir,paste0("Overview_ConditionNumbers_",diff.cutoff,"_",pvaladj.cutoff,"_",minG,"_",maxG,".png")))
+  write.table(CN,file.path(output_dir,paste0("Overview_ConditionNumbers_",diff.cutoff,"_",pvaladj.cutoff,"_",minG,"_",maxG,".txt")))
+
+  Gall <- which.min(conditionNumbers) + minG-1 # The optimal number of DE genes selected
+  cat(paste("The optimal number of genes for the signature is",Gall,"\n"))
+
+  # Build final signature matrix with the optimal number of genes
+  IDGenes<-c()
+  for(Ind in 1:length(IDsCells)){
+    temp <- eval(parse(text = as.name(paste0("DE_filt.",IDsCells[Ind]))))
+    temp <- temp[order(temp$avg_log2FC,decreasing=TRUE),]
+    IDGenes <- c(IDGenes,(rownames(temp)[1:min(Gall,NGenes[Ind])]))
+   }
+
+  IDGenes <- unique(IDGenes)
+  ExprSubset <- GetAssayData(expression[IDGenes,], assay.type = "RNA", slot = "counts")
+  Sig <- c()
+   for(celltype in IDsCells){
+     Sig <- cbind(Sig,(apply(ExprSubset,1,function(y) mean(y[which(clusters==celltype)])))) #to try both median and mean
+   }
+  colnames(Sig) <- IDsCells
+  write.table(Sig,file.path(output_dir,paste0("Signature_",NameSig,"_",diff.cutoff,"_",pvaladj.cutoff,"_",minG,"_",maxG,".txt")),sep="\t",row.names=TRUE,col.names=NA)
+  png(file.path(output_dir,paste0("Signature_",NameSig,"_",diff.cutoff,"_",pvaladj.cutoff,"_",minG,"_",maxG,".png")),units='mm',height=550,res = 300)
+    a <- heatmap(Sig)
+  dev.off()
+  return(Sig)
+}
+
+
